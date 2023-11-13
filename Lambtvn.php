5af2de328c00dbdab1199a8c1489f65877468cc3
Huy <huy27082003@gmail.com>
2023-11-13 16:46:16 +0700

    Sua l2

---
+++ Lambtvn.php
@@ -0,0 +1,31 @@
+<!-- Viết chương trình PHP để kiểm tra xem một số có phải là số chẵn hay không.
+Viết chương trình PHP để tính tổng của các số từ 1 đến n.
+Viết chương trình PHP để in ra bảng cửu chương từ 1 đến 10.
+Viết chương trình PHP để kiểm tra xem một chuỗi có chứa một từ cụ thể hay không.
+Viết chương trình PHP để tìm giá trị lớn nhất và nhỏ nhất trong một mảng.
+Viết chương trình PHP để sắp xếp một mảng theo thứ tự tăng dần.
+Viết chương trình PHP để tính giai thừa của một số nguyên dương.
+Viết chương trình PHP để tìm số nguyên tố trong một khoảng cho trước.
+Viết chương trình PHP để tính tổng của các số trong một mảng.
+Viết chương trình PHP để in ra các số Fibonacci trong một khoảng cho trước.
+Viết chương trình PHP để kiểm tra xem một số có phải là số Armstrong hay không.
+Viết chương trình PHP để chèn một phần tử vào một mảng ở vị trí bất kỳ.
+Viết chương trình PHP để loại bỏ các phần tử trùng lặp trong một mảng.
+Viết chương trình PHP để tìm vị trí của một phần tử trong một mảng.
+Viết chương trình PHP để đảo ngược một chuỗi.
+Viết chương trình PHP để tính số lượng phần tử trong một mảng.
+Viết chương trình PHP để kiểm tra xem một chuỗi có phải là chuỗi Palindrome hay không.
+Viết chương trình PHP để đếm số lần xuất hiện của một phần tử trong một mảng.
+Viết chương trình PHP để sắp xếp một mảng theo thứ tự giảm dần.
+Viết chương trình PHP để thêm một phần tử vào đầu hoặc cuối của một mảng.
+Viết chương trình PHP để tìm số lớn thứ hai trong một mảng.
+Viết chương trình PHP để tìm ước số chung lớn nhất và bội số chung nhỏ nhất của hai số nguyên dương.
+Viết chương trình PHP để kiểm tra xem một số có phải là số hoàn hảo hay không.
+Viết chương trình PHP để tìm số lẻ lớn nhất trong một mảng.
+Viết chương trình PHP để kiểm tra xem một số có phải là số nguyên tố hay không.
+Viết chương trình PHP để tìm số dương lớn nhất trong một mảng.
+Viết chương trình PHP để tìm số âm lớn nhất trong một mảng.
+Viết chương trình PHP để tính tổng các số lẻ từ 1 đến n.
+Viết chương trình PHP để tìm số chính phương trong một khoảng cho trước.
+Viết chương trình PHP để kiểm tra xem một chuỗi có phải là chuỗi con của một chuỗi khác hay không. -->
+echo "sau sua";
