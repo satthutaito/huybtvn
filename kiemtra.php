d30ac9e36371a66c9b7ca0f42ecfd3bae82b24ee
Huy <huy27082003@gmail.com>
2023-11-14 09:28:45 +0700

    Bai Kiem Tra

---
+++ kiemtra.php
@@ -0,0 +1,69 @@
+<!-- C1: Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 [4] => 5 [5] => 6 [6] => 9 )
+C2: B.TRUE
+C3: A.array()
+C4: A.readfile()
+C5: B.Chuyển đổi một đoios tượng thành một chuỗi -->
+
+<!-- Bài 1: Viết một chương trình PHP để hiển thị dãy số Fibonacci. Yêu cầu:
+Tạo một hàm generateFibonacci nhận một số nguyên dương n và trả về dãy số Fibonacci đầu tiên có n phần tử.
+Hiển thị dãy số Fibonacci đầu tiên có 10 phần tử. -->
+
+<?php
+function generateFibonacci($n) {
+    $fibonacci = array();
+    $fibonacci[0] = 0;
+    $fibonacci[1] = 1;
+
+    for ($i = 2; $i < $n; $i++) {
+        $fibonacci[$i] = $fibonacci[$i-1]+$fibonacci[$i-2];
+    }
+    return $fibonacci;
+}
+$n = 10;
+
+$ketqua = generateFibonacci($n);
+
+echo "Dãy số Fibonacci đầu tiên có $n phần tử là: " . implode(", ", $ketqua);
+?>
+
+<!-- Bài 2: Viết một ứng dụng PHP để quản lý thông tin về học sinh trong một lớp học sử dụng mảng kết hợp. Yêu cầu:
+Tạo một mảng kết hợp chứa thông tin về học sinh với các khóa như id, name, age, và grade.
+Hiển thị thông tin của tất cả học sinh trong mảng.
+Viết hàm để tìm học sinh có điểm cao nhất (grade). -->
+
+
+<?php
+
+// Tạo mảng kết hợp chứa thông tin học sinh
+$students = array(
+    array("id" => 1, "name" => "Nguyen Van A", "age" => 20, "grade" => 85),
+    array("id" => 2, "name" => "Tran Thi B", "age" => 21, "grade" => 92),
+    array("id" => 3, "name" => "Le Van C", "age" => 19, "grade" => 78),
+);
+
+echo "<br><br><br>-----Thông tin của tất cả học sinh:<br>";
+foreach ($students as $student) {
+    echo "ID: " . $student['id'] . ", Name: " . $student['name'] . ", Age: " . $student['age'] . ", Grade: " . $student['grade'] ,"<br>";
+}
+
+// Viết hàm để tìm học sinh có điểm cao nhất
+function findHighestGradeStudent($students) {
+    $diem = 0;
+    $hocsinh = null;
+
+    foreach ($students as $student) {
+        if ($student['grade'] > $diem) {
+            $diem = $student['grade'];
+            $hocsinh = $student;
+        }
+    }
+
+    return $hocsinh;
+}
+
+// Gọi hàm và hiển thị thông tin học sinh có điểm cao nhất
+$hocsinh = findHighestGradeStudent($students);
+
+echo "-----Học sinh có điểm cao nhất:<br>";
+echo "ID: " . $hocsinh['id'] . ", Name: " . $hocsinh['name'] . ", Age: " . $hocsinh['age'] . ", Grade: " . $hocsinh['grade'] . "\n";
+?>
